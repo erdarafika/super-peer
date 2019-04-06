c5be71fe3f7351f03fab01ea64386ab01c9c056b
unknown <muhanwar@digiro.in>
2019-04-06 15:02:20 +0700

    Fix validation

--- server.js
+++ server.js
@@ -55,9 +55,7 @@ Gun.on('opt', function(ctx){
                         if (pubkey == 'undefined' || pubkey.length < 87) {
                             
                         } else {
-                            console.log(JSON.stringify({m: {message: obj.message}, s: obj.sig}))
-                            const sig = 'SEA{"m":{"message":"'+ obj.message +'"},"s":"'+ obj.sig +'"}'
-                            console.log(sig)
+                            const sig = "SEA"+JSON.stringify({m: {message: obj.message}, s: obj.sig})
                             verify_sig(sig, pubkey).then( res => {
                                 const post = res.message
                                 try {
