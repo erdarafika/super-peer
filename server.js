7d9873a80539cb7c9b9381f9c849d7512e80a1d7
unknown <muhanwar@digiro.in>
2019-04-06 14:53:04 +0700

    Fix validation

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ Gun.on('opt', function(ctx){
                         if (pubkey == 'undefined' || pubkey.length < 87) {
                             
                         } else {
-                            console.log(obj)
+                            console.log(pub.val)
                             const sig = 'SEA{"m":{"message":"'+ obj.message +'"},"s":"'+ obj.sig +'"}'
                             console.log(sig)
                             verify_sig(sig, pubkey).then( res => {
